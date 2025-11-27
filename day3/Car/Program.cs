7e533b774f2ace68a8259e0a502bed49e5023cdb
Javohir <javohirsadullayev2024@gmail.com>
2025-11-27 21:39:05 +0500

    day3

---
+++ day3/Car/Program.cs
@@ -0,0 +1,35 @@
+﻿class Car
+{
+  private int _speed;
+
+  public string Brand {get;set;}
+  public int Speed
+  {
+    get => _speed;
+    set
+    {
+      if (value < 0)
+        throw new ArgumentException("Speed manfiy bo'lmasligi kerak");
+      else if (value > 300)
+        throw new ArgumentException("Speed 300 dan o'tmasligi kerak");
+      _speed = value;
+    }
+  }
+
+  public void PrintInfo()
+  {
+    Console.WriteLine($"Mashina brandi: {Brand}, Speed: {Speed}");
+  }
+}
+
+class Program
+{
+  static void Main()
+  {
+    Car car = new Car();
+    car.Speed = 301;
+    car.Brand = "BMW";
+    car.PrintInfo();
+  }
+}
+
